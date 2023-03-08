3c3f7690414479f84ad8bd9a5081a3c154f3fd3d
PhamXuanLam <xuanlam21062002@gmail.com>
2023-03-08 11:44:04 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Chị Bé';
+const giftUrl = 'https://www.facebook.com/Hauskull27596';
+const eventName = 'Mừng ngày 8-3';
+const titleCard = 'Gửi người con gái trong lòng tôi';
+const contentCard = 'Chúc chị nhung gấm lụa là, tay cầm tiền tỉ tự làm đại gia. Chúc chị trong túi có tiền, trong tay có việc trong lòng có tuii !';
+
+const giftImage = 'mon-avenir.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
